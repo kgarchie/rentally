b510a8c12694fd181097565c459c1ef0add1744b
kgarchie <archiethebig@gmail.com>
2022-01-31 11:22:59 +0300

    Added Procfile for windows

--- rentally/settings.py
+++ rentally/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,3 +136,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # LIVERELOAD_PORT = '8000'
 # LIVERELOAD_HOST = '127.0.0.1'
+
+import django_heroku
+django_heroku.settings(locals())
+
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
